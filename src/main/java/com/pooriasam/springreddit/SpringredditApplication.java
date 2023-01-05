64dd354cca23b9afb887e23df85757b31905c9e1
newrochi <yusufyuzlu2008@gmail.com>
2023-01-05 02:31:41 -0400

    project started

---
+++ src/main/java/com/pooriasam/springreddit/SpringredditApplication.java
@@ -0,0 +1,13 @@
+package com.pooriasam.springreddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringredditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringredditApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pooriasam/springreddit/SpringredditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pooriasam.springreddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringredditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
